fa57caeb727a4a65796cb34994db8137c883980c
rstavchansky <rachael@netlify.com>
2023-04-25 20:17:17 -0400

    first files, variables

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// selects the ul element where the guessed letters will go
+const guesses = document.querySelector(".guessed-letters");
+// selects the Guess button
+const guessButton = document.querySelector(".guess");
+// selects text input for the letter
+const letter = document.querySelector("#letter");
+// selects empty paragraph for word in progress
+const progress = document.querySelector(".word-in-progress");
+// selects paragraph for remaining guesses
+const remaining = document.querySelector(".remaining")
+// selects span inside remaining
+const guessesLeft = document.querySelector(".guesses-left");
+// selects message 
+const message = document.querySelector(".message");
+// button to play again
+const replayButton = document.querySelector(".play-again");
+// test word
+const word = "magnolia";
