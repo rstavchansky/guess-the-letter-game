42dd4578b281ed5c432be053c545916851c627dd
rstavchansky <rachael@netlify.com>
2023-04-29 17:37:00 -0400

    add the guessing part

--- js/script.js
+++ js/script.js
@@ -16,12 +16,14 @@ const message = document.querySelector(".message");
 const replayButton = document.querySelector(".play-again");
 // test word
 const word = "magnolia";
+// array for guessed letters
+const guessedLetters = [];
 
 //function to update words in progress with circles
 const swapCircles = function (word) {
     const mysteryLetters = [];
     for (const letter of word) {
-        console.log(letter);
+        //console.log(letter);
         mysteryLetters.push("●");
     }
     letter.innerText = mysteryLetters.join("");
@@ -32,4 +34,34 @@ guessButton.addEventListener("click", function (e) {
     const input = letter.value;
     console.log(input);
     letter.value = "";
+    message.innerText = "";
+    const validatedInput = validateInput(input);
+    //console.log(validatedInput);
+    if (validatedInput) {
+    // It's valid.
+    makeGuess(validatedInput);
+    };
 });
+
+const validateInput = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0) {
+        message.innerText = "Please enter a letter";
+    } else if (input.length > 1) {
+       message.innerText = "Did you enter more than one letter? Just enter one."
+    } else if (!input.match(acceptedLetter)) {
+        message.innerText = "Please enter a letter only"
+    } else {
+        return input;
+    }
+};
+
+const makeGuess = function (validatedInput) {
+    validatedInput = validatedInput.toUpperCase();
+    if (guessedLetters.includes(validatedInput)) {
+        message.innerText = "You already guessed that, sorry"
+    } else {
+        guessedLetters.push(validatedInput);
+        console.log(guessedLetters);
+    }
+};
